80bcaa9ed0a8dfbc55d25a9948f437159aac86f9
Karell Ste-Marie <karell.stemarie@gmail.com>
2012-09-26 12:21:12 -0400

    Initial version, unit test passes

---
+++ src/Karell.PerfCounters.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Karell.PerfCounters.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var pm = new PerformanceCounterManager("Multi Counter Category", "Category help");
+            pm.RegisterCounter("Counter1", "help string1", PerformanceCounterType.NumberOfItems64);
+            pm.RegisterCounters();
+            Do2("Counter1", pm);
+            pm.DeregisterCounters();
+        }
+
+        private static void Do2(string counterName, PerformanceCounterManager manager)
+        {
+            using (var c = manager.GetCounter(counterName))
+            {
+                Parallel.For(1, 10, a => DoYourThing(c));
+
+                c.Close();
+            }
+        }
+
+        private static void DoYourThing(PerformanceCounter c)
+        {
+            c.Increment();
+            Thread.Sleep(5);
+            c.Decrement();
+        }
+    }
+}
---
+++ src/Karell.PerfCounters/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Karell.PerfCounters
+{
+    public class PerformanceCounterManager
+    {
+        private readonly CounterCreationDataCollection _collection =
+            new CounterCreationDataCollection();
+        private readonly Dictionary<string, CounterCreationData> _items =
+            new Dictionary<string, CounterCreationData>();
+        private readonly string _categoryName;
+        private readonly string _categoryHelp;
+
+        public PerformanceCounterManager(string categoryName, string categoryHelp)
+        {
+            _categoryName = categoryName;
+            _categoryHelp = categoryHelp;
+        }
+
+        public void RegisterCounter(string counterName, string helpString, PerformanceCounterType performanceCounterType)
+        {
+            CounterCreationData counter =
+                new CounterCreationData
+                    {
+                        CounterName = counterName,
+                        CounterHelp = helpString,
+                        CounterType = performanceCounterType
+                    };
+
+            _collection.Add(counter);
+            _items.Add(counterName, counter);
+        }
+
+        public void DeregisterCounter(string counterName)
+        {
+            _collection.Remove(_items[counterName]);
+            _items.Remove(counterName);
+        }
+
+        public IList<string> CounterNames
+        {
+            get { return _items.Keys.ToList(); }
+        }
+
+        public bool RegisterCounters()
+        {
+            if (!PerformanceCounterCategory.Exists(_categoryName))
+            {
+                PerformanceCounterCategory.Create(
+                    _categoryName, _categoryHelp,
+                    PerformanceCounterCategoryType.SingleInstance, _collection);
+                return true;
+            }
+            return false;
+        }
+
+        public void DeregisterCounters()
+        {
+            PerformanceCounterCategory.Delete(_categoryName);
+        }
+
+        public void UpdateCounters()
+        {
+            DeregisterCounters();
+            RegisterCounters();
+        }
+
+        public PerformanceCounter GetCounter(string counterName)
+        {
+            if (_items.ContainsKey(counterName))
+                return new PerformanceCounter(_categoryName, counterName, false);
+            throw new ArgumentException("Counter name does not exist");
+        }
+    }
+}
